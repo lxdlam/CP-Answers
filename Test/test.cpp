33c38f9917b67a3bcca815a77c0d86abe217d876
lxdlam <lxdlam@gmail.com>
2017-03-29 23:23:57 +0800

    gitignore fixing

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,10 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <string>
+
+using namespace std;
+
 string reverse(string src)
 {
     string res = "";
-    for (auto it = src.begin(); it != src.end(); it++)
-        res += *it;
+    for (int i = src.size() - 1; i >= 0; i--)
+        res += src[i];
     return res;
+}
+
+TEST_CASE("reverse")
+{
+    REQUIRE(reverse("") == "");
+    REQUIRE(reverse("A") == "A");
+    REQUIRE(reverse("123456789") == "987654321");
 }
