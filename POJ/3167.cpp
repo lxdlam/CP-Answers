518c8c8fef1554d4ab2dc70fddecceb5a87e41e4
lxdlam <lxdlam@gmail.com>
2017-10-03 17:24:45 +0800

    Regular Commit

--- POJ/3167.cpp
+++ POJ/3167.cpp
@@ -1,34 +1,90 @@
+//WA
 #include <iostream>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-void getNext(const string &s, int next[])
+typedef vector<int> vi;
+
+vi t, p, tls, pls, tgt, pgt, ans;
+
+void pre(vi &lt, vi &gt, vi &v)
 {
-    int j;
-    j = next[0] = -1;
-    for (int i = 0; i < s.size(); i++)
+    for (int i = 0; i < p.size(); i++)
     {
-        while (j != -1 && s[i] != s[j])
-            j = next[j];
-        next[i + 1] = ++j;
+        lt.push_back(0);
+        gt.push_back(0);
+        for (int j = 0; j < i; j++)
+        {
+            if (v[j] < v[i])
+                lt[i]++;
+            else if (v[j] > v[i])
+                gt[i]++;
+        }
     }
 }
 
-int match()
+//kmp
+void getNext(vi &next)
+{
+    int j;
+    next.push_back(-1);
+    j = next[0];
+    for (int i = 0; i < p.size(); i++)
+    {
+        while (j != -1 && (pls[i] != pls[j] || pgt[i] != pgt[j]))
+            j = next[j];
+        next.push_back(++j);
+    }
+}
+
+void match()
+{
+    vi next;
+    int count = 0;
+    int j = 0;
+    getNext(next);
+    for (int i = 0; i < t.size(); i++)
+    {
+        while (j != -1 && (tls[i] != pls[j] || tgt[i] != pgt[j]))
+            j = next[j];
+        j++;
+        if (j == p.size())
+        {
+            ans.push_back(i - p.size() + 1);
+            j = next[j];
+        }
+    }
+}
+
+void solve()
+{
+    pre(tls, tgt, t);
+    pre(pls, pgt, p);
+    match();
+}
 
 int main()
 {
-    int next[100] = {0};
-    string temp;
-    while (cin >> temp &&temp != "-1")
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k, s, tmp;
+    cin >> n >> k >> s;
+    while (n--)
     {
-        getNext(temp, next);
-        for (int i = 0; i <= temp.size(); i++)
-        {
-            cout << next[i] << " ";
-        }
-        cout << endl;
+        cin >> tmp;
+        t.push_back(tmp);
+    }
+    while (k--)
+    {
+        cin >> tmp;
+        p.push_back(tmp);
+    }
+    solve();
+    cout << ans.size() << endl;
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << endl;
     }
     return 0;
 }
