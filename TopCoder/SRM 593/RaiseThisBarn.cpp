9662f930f527188be2152eacb7a8edfaffd7caea
lxdlam <lxdlam@gmail.com>
2017-10-29 20:29:14 +0800

    Regular

--- TopCoder/SRM 593/RaiseThisBarn.cpp
+++ TopCoder/SRM 593/RaiseThisBarn.cpp
@@ -2,24 +2,26 @@
 
 using namespace std;
 
-class RaiseThisBarn {
-    public:
-    int calc(string str) {
+class RaiseThisBarn
+{
+  public:
+    int calc(string str)
+    {
         int count = 0, res = 0, k = 0;
-        for(auto i: str)
-            if(i == 'c')
+        for (auto i : str)
+            if (i == 'c')
                 count++;
-        if(count % 2)
+        if (count % 2)
             return 0;
-        else if(count == 0)
-            return str.size()-1;
-        for(int i=0;i<str.size();i++)
+        else if (count == 0)
+            return str.size() - 1;
+        for (int i = 0; i < str.size(); i++)
         {
-            if(str[i] == 'c')
+            if (str[i] == 'c')
                 k++;
-            if(k*2 == count)
+            if (k * 2 == count)
             {
-                for(int j = i + 1; str[j]!='c'; j++)
+                for (int j = i + 1; str[j] != 'c'; j++)
                     res++;
                 break;
             }
