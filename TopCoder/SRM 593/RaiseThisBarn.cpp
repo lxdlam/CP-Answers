8b197560bb3eb55d38451303e534efa127a1f24c
lxdlam <lxdlam@gmail.com>
2017-10-29 16:07:52 +0800

    Regular commit

---
+++ TopCoder/SRM 593/RaiseThisBarn.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class RaiseThisBarn {
+    public:
+    int calc(string str) {
+        int count = 0, res = 0, k = 0;
+        for(auto i: str)
+            if(i == 'c')
+                count++;
+        if(count % 2)
+            return 0;
+        else if(count == 0)
+            return str.size()-1;
+        for(int i=0;i<str.size();i++)
+        {
+            if(str[i] == 'c')
+                k++;
+            if(k*2 == count)
+            {
+                for(int j = i + 1; str[j]!='c'; j++)
+                    res++;
+                break;
+            }
+        }
+        return res + 1;
+    }
+};
