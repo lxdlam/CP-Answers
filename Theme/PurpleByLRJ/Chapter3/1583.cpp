9240050fc1b39d2f0aa40bf72b31455853a3819c
lxdlam <lxdlam@gmail.com>
2017-11-27 00:02:45 +0800

    Fix indent

--- Theme/PurpleByLRJ/Chapter3/1583.cpp
+++ Theme/PurpleByLRJ/Chapter3/1583.cpp
@@ -6,28 +6,28 @@ int table[MAXN] = {0};
 
 void init()
 {
-  for (int i = 1; i < MAXN; i++)
-  {
-    int x = i, y = i;
-    while (x)
+    for (int i = 1; i < MAXN; i++)
     {
-      y += x % 10;
-      x /= 10;
+        int x = i, y = i;
+        while (x)
+        {
+            y += x % 10;
+            x /= 10;
+        }
+        if (table[y] == 0 || i < table[y])
+            table[y] = i;
     }
-    if (table[y] == 0 || i < table[y])
-      table[y] = i;
-  }
 }
 
 int main()
 {
-  init();
-  int t, q;
-  scanf("%d", &t);
-  while (t--)
-  {
-    scanf("%d", &q);
-    printf("%d\n", table[q]);
-  }
-  return 0;
+    init();
+    int t, q;
+    scanf("%d", &t);
+    while (t--)
+    {
+        scanf("%d", &q);
+        printf("%d\n", table[q]);
+    }
+    return 0;
 }
--- Theme/PurpleByLRJ/Chapter3/340.cpp
+++ Theme/PurpleByLRJ/Chapter3/340.cpp
@@ -6,37 +6,37 @@ const int size = 1e4 + 5;
 
 int main()
 {
-  int games = 0, t, ans[size], guess[size], A, B, c1, c2;
-  while (scanf("%d", &t) && t)
-  {
-    printf("Game %d:\n", ++games);
-    for (int i = 0; i < t; i++)
-      scanf("%d", &ans[i]);
-    for (;;)
+    int games = 0, t, ans[size], guess[size], A, B, c1, c2;
+    while (scanf("%d", &t) && t)
     {
-      A = B = 0;
-      for (int i = 0; i < t; i++)
-      {
-        scanf("%d", &guess[i]);
-        if (ans[i] == guess[i])
-          A++;
-      }
-      if (guess[0] == 0)
-        break;
-      for (int i = 0; i <= 9; i++)
-      {
-        c1 = c2 = 0;
-        for (int j = 0; j < t; j++)
+        printf("Game %d:\n", ++games);
+        for (int i = 0; i < t; i++)
+            scanf("%d", &ans[i]);
+        for (;;)
         {
-          if (ans[j] == i)
-            c1++;
-          if (guess[j] == i)
-            c2++;
+            A = B = 0;
+            for (int i = 0; i < t; i++)
+            {
+                scanf("%d", &guess[i]);
+                if (ans[i] == guess[i])
+                    A++;
+            }
+            if (guess[0] == 0)
+                break;
+            for (int i = 0; i <= 9; i++)
+            {
+                c1 = c2 = 0;
+                for (int j = 0; j < t; j++)
+                {
+                    if (ans[j] == i)
+                        c1++;
+                    if (guess[j] == i)
+                        c2++;
+                }
+                B += c1 > c2 ? c2 : c1;
+            }
+            printf("    (%d,%d)\n", A, B - A);
         }
-        B += c1 > c2 ? c2 : c1;
-      }
-      printf("    (%d,%d)\n", A, B - A);
     }
-  }
-  return 0;
+    return 0;
 }
