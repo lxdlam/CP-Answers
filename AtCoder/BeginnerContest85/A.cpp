1607da68f91d6891440c6db18d299031518dc449
lxdlam <lxdlam@gmail.com>
2018-01-10 10:43:17 +0800

    Optimize Folder Structure

---
+++ AtCoder/BeginnerContest85/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    cin >> s;
+    s[3] = '8';
+    cout << s << endl;
+    return 0;
+}
