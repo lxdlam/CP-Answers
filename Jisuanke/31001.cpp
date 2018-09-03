f22de140e213564ad04ab9a28c503606f00d11a4
lxdlam <lxdlam@gmail.com>
2018-09-03 15:02:13 +0800

    Add Linear Basis

---
+++ Jisuanke/31001.cpp
@@ -0,0 +1,266 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/priority_queue.hpp>
+
+using namespace std;
+
+#define TemplateVersion "3.4.1"
+// Useful Marcos
+//====================START=====================
+// Compile use C++11 and above
+#ifdef LOCAL
+#define debug(args...)                           \
+    {                                            \
+        string _s = #args;                       \
+        replace(_s.begin(), _s.end(), ',', ' '); \
+        stringstream _ss(_s);                    \
+        istream_iterator<string> _it(_ss);       \
+        err(_it, args);                          \
+    }
+void err(istream_iterator<string> it)
+{
+}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+    cerr << *it << " = " << a << endl;
+    err(++it, args...);
+}
+#define MSG cout << "Finished" << endl
+#else
+#define debug(args...)
+#define MSG
+#endif
+#if __cplusplus >= 201703L
+template <typename... Args>
+void readln(Args &... args)
+{
+    ((cin >> args), ...);
+}
+template <typename... Args>
+void writeln(Args... args)
+{
+    ((cout << args << " "), ...);
+    cout << endl;
+}
+#elif __cplusplus >= 201103L
+void readln()
+{
+}
+template <typename T, typename... Args>
+void readln(T &a, Args &... args)
+{
+    cin >> a;
+    readln(args...);
+}
+void writeln()
+{
+    cout << endl;
+}
+template <typename T, typename... Args>
+void writeln(T a, Args... args)
+{
+    cout << a << " ";
+    writeln(args...);
+}
+#endif
+#if __cplusplus >= 201103L
+#define FOR(_i, _begin, _end) for (auto _i = _begin; _i < _end; _i++)
+#define FORR(_i, _begin, _end) for (auto _i = _begin; _i > _end; _i--)
+#else
+#define FOR(_i, _begin, _end) for (int _i = (int)_begin; _i < (int)_end; _i++)
+#define FORR(_i, _begin, _end) for (int _i = (int)_begin; _i > (int)_end; _i--)
+#define nullptr NULL
+#endif
+#if __cplusplus >= 201103L
+#define VIS(_kind, _name, _size) \
+    vector<_kind> _name(_size);  \
+    for (auto &i : _name)        \
+        cin >> i;
+#else
+#define VIS(_kind, _name, _size)    \
+    vector<_kind> _name;            \
+    _name.resize(_size);            \
+    for (int i = 0; i < _size; i++) \
+        cin >> _name[i];
+#endif
+// alias
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define tcase() \
+    int T;      \
+    cin >> T;   \
+    FOR(kase, 1, T + 1)
+// Swap max/min
+template <typename T>
+bool smax(T &a, const T &b)
+{
+    if (a > b)
+        return false;
+    a = b;
+    return true;
+}
+template <typename T>
+bool smin(T &a, const T &b)
+{
+    if (a < b)
+        return false;
+    a = b;
+    return true;
+}
+// ceil divide
+template <typename T>
+T cd(T a, T b)
+{
+    return (a + b - 1) / b;
+}
+// min exchange
+template <typename T>
+bool se(T &a, T &b)
+{
+    if (a < b)
+        return false;
+    swap(a, b);
+    return true;
+}
+// A better MAX choice
+const int INF = 0x3f3f3f3f;
+const long long INFLL = 0x3f3f3f3f3f3f3f3fLL;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef vector<string> cb;
+//====================END=====================
+
+// Constants here
+const int SIZE = 2e5 + 10; // Number of edges
+
+struct Edge
+{
+    ll next, to, w;
+
+    Edge() : next(0), to(0), w(0) {}
+};
+
+Edge E[SIZE];
+int last[SIZE];
+int cnt = 0;
+
+ll dis[SIZE][20];
+
+void add_edge(int u, int v, int w = 1)
+{
+    E[++cnt].next = last[u];
+    E[cnt].to = v;
+    E[cnt].w = w;
+    last[u] = cnt;
+}
+
+struct atom
+{
+    ll x, y, k;
+};
+
+struct cmp
+{
+    bool operator()(const atom &a, const atom &b)
+    {
+        return a.x == b.x ? (a.y > b.y) : (a.x > b.x);
+    }
+};
+
+using pqat = __gnu_pbds::priority_queue<atom, cmp, __gnu_pbds::pairing_heap_tag>;
+
+void init()
+{
+    memset(E, 0, sizeof E);
+    memset(last, 0, sizeof last);
+    memset(dis, 0x3f, sizeof dis);
+    cnt = 0;
+}
+
+void dijkstra(int n, int k)
+{
+    pqat pq;
+
+    dis[1][0] = 0;
+    pq.push({0, 1, 0});
+
+    while (pq.size())
+    {
+        auto a = pq.top();
+        pq.pop();
+        int x = a.y;
+        int c = a.k;
+        if (dis[x][c] < a.x)
+            continue;
+        for (int i = last[x]; i; i = E[i].next)
+        {
+            int t = E[i].to;
+            if (dis[t][c] > dis[x][c] + E[i].w)
+            {
+                dis[t][c] = dis[x][c] + E[i].w;
+                pq.push({dis[t][c], t, c});
+            }
+            if (c + 1 <= k)
+            {
+                // Set distance to 0
+                // The next layer of the graph
+                if (dis[t][c + 1] > dis[x][c])
+                {
+                    dis[t][c + 1] = dis[x][c];
+                    pq.push({dis[t][c + 1], t, c + 1});
+                }
+            }
+        }
+    }
+}
+
+// Pre-Build Function
+inline void build()
+{
+}
+
+// Actual Solver
+inline void solve()
+{
+    tcase()
+    {
+        init();
+        int n, m, k;
+        readln(n, m, k);
+        int u, v, p;
+        while (m--)
+        {
+            readln(u, v, p);
+            add_edge(u, v, p);
+        }
+        dijkstra(n, k);
+        cout << dis[n][k] << '\n';
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+#ifdef LOCAL
+    clock_t _begin = clock();
+#endif
+
+    build();
+    solve();
+
+#ifdef LOCAL
+    cerr << "Time elapsed: " << (double)(clock() - _begin) * 1000 / CLOCKS_PER_SEC << "ms." << endl;
+#endif
+
+    return 0;
+}
---
+++ My Templates/LinearBasis.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct LB
+{
+    using ll = long long;
+    int MaxL;
+
+    vector<ll> data;
+
+    LB(int size = 60)
+    {
+        MaxL = size;
+        data.resize(size + 1);
+    }
+
+    void insert(ll t)
+    {
+        for (int j = MaxL; j >= 0; j--)
+        {
+            if (!(t & (1LL << j)))
+                continue;
+
+            if (data[j])
+                t ^= data[j];
+            else
+            {
+                for (int k = 0; k < j; k++)
+                    if (t & (1LL << k))
+                        t ^= data[k];
+
+                for (int k = j + 1; k <= MaxL; k++)
+                    if (data[k] & (1LL << j))
+                        data[k] ^= t;
+
+                data[j] = t;
+
+                return;
+            }
+        }
+    }
+
+    void merge(const LB &l)
+    {
+        for (int i = 0; i <= MaxL; i++)
+            insert(l.data[i]);
+    }
+
+    ll queryMax()
+    {
+        ll ans = 0;
+        for (auto i : data)
+            ans ^= i;
+        return ans;
+    }
+
+    int rank()
+    {
+        int ans = 0;
+        for (auto i : data)
+            if (i)
+                ans++;
+        return ans;
+    }
+};
