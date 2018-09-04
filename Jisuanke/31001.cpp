8b6e336e81eb9178d5da49eae613041c32696444
lxdlam <lxdlam@gmail.com>
2018-09-04 15:38:40 +0800

    Add Fast IO template, thx to https://oi-wiki.org/misc/io/

--- Jisuanke/31001.cpp
+++ Jisuanke/31001.cpp
@@ -138,6 +138,54 @@ typedef vector<string> cb;
 //====================END=====================
 
 // Constants here
+namespace io
+{
+const int MAXSIZE = 1 << 22;
+inline char gc()
+{
+    static char In[MAXSIZE], *at = In, *en = In;
+    if (at == en)
+        en = (at = In) + fread(In, 1, MAXSIZE, stdin);
+    return at == en ? EOF : *at++;
+}
+template <class T>
+inline void read(T &x)
+{
+    char c;
+    while (c = gc(), !isdigit(c) && c != '-')
+        ;
+    bool f = c == '-';
+    x = f ? 0 : c - '0';
+    for (c = gc(); isdigit(c); c = gc())
+        x = x * 10 + c - '0';
+    if (f)
+        x = -x;
+}
+char Out[MAXSIZE], *cur = Out, *end = Out + MAXSIZE - 100;
+void flush()
+{
+    fwrite(Out, 1, cur - Out, stdout);
+    cur = Out;
+}
+template <typename T>
+inline void write(T x, char c = '\n')
+{
+    static int S[20], *top;
+    top = S;
+    if (x < 0)
+        *cur++ = '-', x = -x;
+    do
+    {
+        *++top = x % 10, x /= 10;
+    } while (x);
+    while (top != S)
+        *cur++ = *top-- + '0';
+    *cur++ = c;
+    if (cur >= end)
+        flush();
+}
+} // namespace io
+
 const int SIZE = 2e5 + 10; // Number of edges
 
 struct Edge
@@ -229,27 +277,35 @@ inline void build()
 // Actual Solver
 inline void solve()
 {
-    tcase()
+    using namespace io;
+    int T;
+    read(T);
+    while (T--)
     {
         init();
         int n, m, k;
-        readln(n, m, k);
+        read(n);
+        read(m);
+        read(k);
         int u, v, p;
         while (m--)
         {
-            readln(u, v, p);
+            read(u);
+            read(v);
+            read(p);
             add_edge(u, v, p);
         }
         dijkstra(n, k);
-        cout << dis[n][k] << '\n';
+        write(dis[n][k]);
     }
+    flush();
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
+    // ios::sync_with_stdio(false);
+    // cin.tie(nullptr);
+    // cout.tie(nullptr);
 
 #ifdef LOCAL
     clock_t _begin = clock();
---
+++ My Templates/Fast-IO.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+namespace io
+{
+const int MAXSIZE = 1 << 22;
+inline char gc()
+{
+    static char In[MAXSIZE], *at = In, *en = In;
+    if (at == en)
+        en = (at = In) + fread(In, 1, MAXSIZE, stdin);
+    return at == en ? EOF : *at++;
+}
+template <class T>
+inline void read(T &x)
+{
+    char c;
+    while (c = gc(), !isdigit(c) && c != '-')
+        ;
+    bool f = c == '-';
+    x = f ? 0 : c - '0';
+    for (c = gc(); isdigit(c); c = gc())
+        x = x * 10 + c - '0';
+    if (f)
+        x = -x;
+}
+char Out[MAXSIZE], *cur = Out, *end = Out + MAXSIZE - 100;
+void flush()
+{
+    fwrite(Out, 1, cur - Out, stdout);
+    cur = Out;
+}
+template <typename T>
+inline void write(T x, char c = '\n')
+{
+    static int S[20], *top;
+    top = S;
+    if (x < 0)
+        *cur++ = '-', x = -x;
+    do
+    {
+        *++top = x % 10, x /= 10;
+    } while (x);
+    while (top != S)
+        *cur++ = *top-- + '0';
+    *cur++ = c;
+    if (cur >= end)
+        flush();
+}
+} // namespace io
