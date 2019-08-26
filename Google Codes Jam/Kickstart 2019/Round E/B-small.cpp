6b5af41e95d1f2f22b9ff35400d6d0842edb407c
lxdlam <lxdlam@gmail.com>
2019-08-26 21:56:05 +0800

    Regulat commit

---
+++ Google Codes Jam/Kickstart 2019/Round E/B-small.cpp
@@ -0,0 +1,198 @@
+// Only solved small input
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define TemplateVersion "3.7.1"
+// Useful Marcos
+//====================START=====================
+// Compile use C++11 and above
+#ifdef LOCAL
+#define debug(args...)                       \
+  do {                                       \
+    string _s = #args;                       \
+    replace(_s.begin(), _s.end(), ',', ' '); \
+    stringstream _ss(_s);                    \
+    istream_iterator<string> _it(_ss);       \
+    err(_it, args);                          \
+  } while (0)
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " = " << a << endl;
+  err(++it, args...);
+}
+#define MSG cout << "Finished" << endl
+#else
+#define debug(args...)
+#define MSG
+#endif
+#if __cplusplus >= 201703L
+template <typename... Args>
+void readln(Args &... args) {
+  ((cin >> args), ...);
+}
+template <typename... Args>
+void writeln(Args... args) {
+  ((cout << args << " "), ...);
+  cout << endl;
+}
+#elif __cplusplus >= 201103L
+void readln() {}
+template <typename T, typename... Args>
+void readln(T &a, Args &... args) {
+  cin >> a;
+  readln(args...);
+}
+void writeln() { cout << endl; }
+template <typename T, typename... Args>
+void writeln(T a, Args... args) {
+  cout << a << " ";
+  writeln(args...);
+}
+#endif
+#if __cplusplus >= 201103L
+#define FOR(_i, _begin, _end) for (auto _i = _begin; _i < _end; _i++)
+#define FORR(_i, _begin, _end) for (auto _i = _begin; _i > _end; _i--)
+#else
+#define FOR(_i, _begin, _end) for (int _i = (int)_begin; _i < (int)_end; _i++)
+#define FORR(_i, _begin, _end) for (int _i = (int)_begin; _i > (int)_end; _i--)
+#define nullptr NULL
+#endif
+#if __cplusplus >= 201103L
+#define VIS(_kind, _name, _size) \
+  vector<_kind> _name(_size);    \
+  for (auto &i : _name) cin >> i;
+#else
+#define VIS(_kind, _name, _size) \
+  vector<_kind> _name;           \
+  _name.resize(_size);           \
+  for (int i = 0; i < _size; i++) cin >> _name[i];
+#endif
+// alias
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define clr(x) memset((x), 0, sizeof(x))
+#define infty(x) memset((x), 0x3f, sizeof(x))
+#define tcase() \
+  int T;        \
+  cin >> T;     \
+  FOR(kase, 1, T + 1)
+
+// Swap max/min
+template <typename T>
+bool smax(T &a, const T &b) {
+  if (a > b) return false;
+  a = b;
+  return true;
+}
+template <typename T>
+bool smin(T &a, const T &b) {
+  if (a < b) return false;
+  a = b;
+  return true;
+}
+// ceil divide
+template <typename T>
+T cd(T a, T b) {
+  return (a + b - 1) / b;
+}
+// min exchange
+template <typename T>
+bool se(T &a, T &b) {
+  if (a < b) return false;
+  swap(a, b);
+  return true;
+}
+// A better MAX choice
+const int INF = 0x3f3f3f3f;
+const long long INFLL = 0x3f3f3f3f3f3f3f3fLL;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef vector<string> cb;
+//====================END=====================
+
+// Constants here
+const int SIZE = 1e5 + 10;
+ll C[SIZE], E[SIZE];
+ll A[SIZE], B[SIZE];
+
+// Pre-Build Function
+inline void build() {}
+
+// Actual Solver
+inline void solve() {
+  tcase() {
+    cout << "Case #" << kase << ": ";
+    int D, S;
+    readln(D, S);
+    for (int i = 0; i < S; i++) {
+      cin >> C[i] >> E[i];
+    }
+    for (int i = 0; i < D; i++) {
+      cin >> A[i] >> B[i];
+    }
+
+    if (S == 1) {
+      for (int i = 0; i < D; i++) {
+        bool flag = false;
+        for (int f1 = 0; f1 <= 100; f1++) {
+          if (f1 * C[0] >= A[i] * 100 && (100 - f1) * E[0] >= B[i] * 100) {
+            flag = true;
+            break;
+          }
+        }
+
+        cout << (flag ? 'Y' : 'N');
+      }
+    } else if (S == 2) {
+      for (int i = 0; i < D; i++) {
+        bool flag = false;
+        for (int f1 = 0; f1 <= 100 && !flag; f1++) {
+          for (int f2 = 0; f2 <= 100 && !flag; f2++) {
+            ll ta = f1 * C[0] + f2 * C[1],
+               tb = (100 - f1) * E[0] + (100 - f2) * E[1];
+
+            if (ta >= A[i] * 100 && tb >= B[i] * 100) {
+              flag = true;
+              break;
+            }
+          }
+        }
+        cout << (flag ? 'Y' : 'N');
+      }
+    } else {
+      // TODO
+      for (int i = 0; i < D; i++) cout << 'N';
+    }
+
+    cout << '\n';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+#ifdef LOCAL
+  clock_t _begin = clock();
+#endif
+
+  build();
+  solve();
+
+#ifdef LOCAL
+  cerr << "Time elapsed: " << (double)(clock() - _begin) * 1000 / CLOCKS_PER_SEC
+       << "ms." << endl;
+#endif
+
+  return 0;
+}
---
+++ Google Codes Jam/Kickstart 2019/Round E/C.cpp
@@ -0,0 +1,205 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define TemplateVersion "3.7.1"
+// Useful Marcos
+//====================START=====================
+// Compile use C++11 and above
+#ifdef LOCAL
+#define debug(args...)                       \
+  do {                                       \
+    string _s = #args;                       \
+    replace(_s.begin(), _s.end(), ',', ' '); \
+    stringstream _ss(_s);                    \
+    istream_iterator<string> _it(_ss);       \
+    err(_it, args);                          \
+  } while (0)
+void err(istream_iterator<string> it) {}
+template <typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+  cerr << *it << " = " << a << endl;
+  err(++it, args...);
+}
+#define MSG cout << "Finished" << endl
+#else
+#define debug(args...)
+#define MSG
+#endif
+#if __cplusplus >= 201703L
+template <typename... Args>
+void readln(Args &... args) {
+  ((cin >> args), ...);
+}
+template <typename... Args>
+void writeln(Args... args) {
+  ((cout << args << " "), ...);
+  cout << endl;
+}
+#elif __cplusplus >= 201103L
+void readln() {}
+template <typename T, typename... Args>
+void readln(T &a, Args &... args) {
+  cin >> a;
+  readln(args...);
+}
+void writeln() { cout << endl; }
+template <typename T, typename... Args>
+void writeln(T a, Args... args) {
+  cout << a << " ";
+  writeln(args...);
+}
+#endif
+#if __cplusplus >= 201103L
+#define FOR(_i, _begin, _end) for (auto _i = _begin; _i < _end; _i++)
+#define FORR(_i, _begin, _end) for (auto _i = _begin; _i > _end; _i--)
+#else
+#define FOR(_i, _begin, _end) for (int _i = (int)_begin; _i < (int)_end; _i++)
+#define FORR(_i, _begin, _end) for (int _i = (int)_begin; _i > (int)_end; _i--)
+#define nullptr NULL
+#endif
+#if __cplusplus >= 201103L
+#define VIS(_kind, _name, _size) \
+  vector<_kind> _name(_size);    \
+  for (auto &i : _name) cin >> i;
+#else
+#define VIS(_kind, _name, _size) \
+  vector<_kind> _name;           \
+  _name.resize(_size);           \
+  for (int i = 0; i < _size; i++) cin >> _name[i];
+#endif
+// alias
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define clr(x) memset((x), 0, sizeof(x))
+#define infty(x) memset((x), 0x3f, sizeof(x))
+#define tcase() \
+  int T;        \
+  cin >> T;     \
+  FOR(kase, 1, T + 1)
+
+// Swap max/min
+template <typename T>
+bool smax(T &a, const T &b) {
+  if (a > b) return false;
+  a = b;
+  return true;
+}
+template <typename T>
+bool smin(T &a, const T &b) {
+  if (a < b) return false;
+  a = b;
+  return true;
+}
+// ceil divide
+template <typename T>
+T cd(T a, T b) {
+  return (a + b - 1) / b;
+}
+// min exchange
+template <typename T>
+bool se(T &a, T &b) {
+  if (a < b) return false;
+  swap(a, b);
+  return true;
+}
+// A better MAX choice
+const int INF = 0x3f3f3f3f;
+const long long INFLL = 0x3f3f3f3f3f3f3f3fLL;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef set<int> si;
+typedef vector<string> cb;
+//====================END=====================
+
+// Constants here
+unordered_map<ll, ll> memo;
+
+const int SIZE = 1e6 + 10;
+
+int prime[SIZE + 1] = {0};
+void getPrime() {
+  for (int i = 2; i <= SIZE; i++) {
+    if (!prime[i]) prime[++prime[0]] = i;
+    for (int j = 1; j <= prime[0] && prime[j] <= SIZE / i; j++) {
+      prime[prime[j] * i] = 1;
+      if (i % prime[j] == 0) break;
+    }
+  }
+}
+
+ll count(ll n) {
+  ll k = n;
+  if (!memo.count(n)) {
+    ll ans = 1;
+
+    for (ll i = 1; i <= prime[0] && prime[i] * prime[i] <= n; i++) {
+      if (n % prime[i] == 0) {
+        ll cnt = 0;
+        ll p = prime[i];
+        while (n % p == 0) {
+          n /= p;
+          cnt++;
+        }
+
+        ans = ans * (cnt + 1);
+      }
+    }
+
+    if (n != 1) ans *= 2;
+    memo[k] = ans;
+  }
+
+  return memo[k];
+}
+
+bool check(ll n) {
+  if (n % 4 == 2) return false;
+  if (n % 4 == 0) return (count(n / 4) + 1) / 2 >= 2;
+  return (count(n) + 1) / 2 >= 2;
+}
+
+// Pre-Build Function
+inline void build() { getPrime(); }
+
+// Actual Solver
+inline void solve() {
+  tcase() {
+    cout << "Case #" << kase << ": ";
+    int l, r;
+    int ans = 0;
+    readln(l, r);
+    for (; l <= r; l++) {
+      if (!check(l)) {
+        ans++;
+      }
+    }
+    cout << ans << '\n';
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+#ifdef LOCAL
+  clock_t _begin = clock();
+#endif
+
+  build();
+  solve();
+
+#ifdef LOCAL
+  cerr << "Time elapsed: " << (double)(clock() - _begin) * 1000 / CLOCKS_PER_SEC
+       << "ms." << endl;
+#endif
+
+  return 0;
+}
